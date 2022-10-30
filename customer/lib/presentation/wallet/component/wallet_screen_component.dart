f402b50e50083cee16b78b4ee39bd47870baf5d6
emmadedayo <emmadenagbe@gmail.com>
2022-10-30 17:02:58 +0100

    New transaction widget ui added

--- customer/lib/presentation/wallet/component/wallet_screen_component.dart
+++ customer/lib/presentation/wallet/component/wallet_screen_component.dart
@@ -3,6 +3,8 @@ import 'package:acoride/core/helper/helper_config.dart';
 import 'package:acoride/core/helper/helper_style.dart';
 import 'package:acoride/data/model/TransactionModel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:iconsax/iconsax.dart';
 
 class TransactionWalletWidget extends StatelessWidget {
   final TransactionModel transactionModel;
@@ -12,63 +14,52 @@ class TransactionWalletWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Colors.white,
-      elevation: 1,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 15),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Opacity(
-                  opacity: 0.75,
-                  child: Text('${transactionModel.createdAt}',
-                      style: HelperStyle.textStyle(
-                          context, HelperColor.black, 12, FontWeight.w400)),
-                ),
+    return GestureDetector(
+      onTap: (){
 
-                const SizedBox(
-                  height: 5,
+      },
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(8)
+        ),
+        margin: const EdgeInsets.only(bottom: 8,left: 5,right: 5).r,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 18),
+        child: Row(
+          children: <Widget>[
+            transactionModel.effect == "cr"?
+            Image.asset("assets/images/incoming-call.png", height: 20,):
+            Image.asset("assets/images/outgoing-call.png", height: 20,),
+            const SizedBox(width: 14,),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children:<Widget>[
+                Text("${HelperConfig.paymentReformat(transactionModel.paymentFor ?? '')}", style: HelperStyle.textStyle(
+                    context, HelperColor.black, 15, FontWeight.w400),
                 ),
-                Text(
-                  "${transactionModel.remark}",
-                  style: HelperStyle.textStyle(
+                const SizedBox(height: 5,),
+                Text("${transactionModel.createdAt}",
+                  style:HelperStyle.textStyle(
                       context, HelperColor.black, 12, FontWeight.w400),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                Opacity(
-                  opacity: 0.75,
-                  child: Text('${transactionModel.paymentRef}',
-                      style: HelperStyle.textStyle(
-                          context, HelperColor.black, 12, FontWeight.w400)),
+
                 ),
               ],
-            ),),
-            Padding(
-              padding: const EdgeInsets.only(right: 3.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text('${HelperConfig.currencyFormat('${transactionModel.amount ?? 0}')}',
-                      style: HelperStyle.textStyle(
-                          context, HelperColor.black, 12, FontWeight.w400)),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  // Icon(
-                  //   Icons.arrow_forward_ios,
-                  //   size: 15,
-                  // )
-                ],
-              ),
+            ),
+            const Spacer(),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children:<Widget>[
+                Text("${HelperConfig.currencyFormat('${transactionModel.amount ?? 0}')}",
+                  style:HelperStyle.textStyle(
+                      context, transactionModel.effect == "cr"?HelperColor.greenColor:HelperColor.redColor, 12, FontWeight.w400),
+
+                ),
+                Text("",
+                  style:HelperStyle.textStyle(
+                      context, HelperColor.black, 12, FontWeight.w400),
+
+                ),
+              ],
             ),
           ],
         ),
